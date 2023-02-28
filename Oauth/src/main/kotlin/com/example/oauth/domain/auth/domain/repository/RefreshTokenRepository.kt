7f640fffda7ef7550e3c3414feb65c8cafdac32d
주영 <jyk102999@dsm.hs.kr>
2023-02-28 16:14:18 +0900

    🔥 :: 코드 정렬

--- Oauth/src/main/kotlin/com/example/oauth/domain/auth/domain/repository/RefreshTokenRepository.kt
+++ Oauth/src/main/kotlin/com/example/oauth/domain/auth/domain/repository/RefreshTokenRepository.kt
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface RefreshTokenRepository:CrudRepository<RefreshToken, String> {
+interface RefreshTokenRepository : CrudRepository<RefreshToken, String> {
 }
