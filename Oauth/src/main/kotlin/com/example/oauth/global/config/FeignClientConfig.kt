c708f8dcfc37c4833ad99832467b44b59556ca31
주영 <jyk102999@dsm.hs.kr>
2023-03-02 00:46:53 +0900

    ⚡️ :: FeignClientConfig 추가

---
+++ Oauth/src/main/kotlin/com/example/oauth/global/config/FeignClientConfig.kt
@@ -0,0 +1,8 @@
+package com.example.oauth.global.config
+
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Configuration
+
+@EnableFeignClients
+@Configuration
+class FeignClientConfig
