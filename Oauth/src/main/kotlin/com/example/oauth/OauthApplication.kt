ea9c9cd7c86b7fdc46535f59ea98be9397390c9d
주영 <jyk102999@dsm.hs.kr>
2023-03-02 02:06:05 +0900

    ⚡️ :: EnableFeignClient 추가

--- Oauth/src/main/kotlin/com/example/oauth/OauthApplication.kt
+++ Oauth/src/main/kotlin/com/example/oauth/OauthApplication.kt
@@ -1,12 +1,15 @@
 package com.example.oauth
 
 import com.example.oauth.global.security.jwt.JwtProperties
+import com.example.oauth.infrastructure.feign.properties.GoogleFeignProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @EnableConfigurationProperties(JwtProperties::class)
+@EnableFeignClients
 class OauthApplication
 
 fun main(args: Array<String>) {
