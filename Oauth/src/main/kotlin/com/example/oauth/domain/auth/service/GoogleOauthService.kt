bd24675400116ce8fe8fee1bebb19d6b858f9e4d
주영 <jyk102999@dsm.hs.kr>
2023-03-02 08:28:22 +0900

    ♻️ :: Facade pattern, passwordEncorder 적용

--- Oauth/src/main/kotlin/com/example/oauth/domain/auth/service/GoogleOauthService.kt
+++ Oauth/src/main/kotlin/com/example/oauth/domain/auth/service/GoogleOauthService.kt
@@ -4,12 +4,14 @@ import com.example.oauth.domain.user.domain.User
 import com.example.oauth.domain.user.domain.repository.UserRepository
 import com.example.oauth.domain.user.domain.type.ProviderType.GOOGLE
 import com.example.oauth.domain.user.domain.type.Role.USER
+import com.example.oauth.domain.user.facade.UserFacade
 import com.example.oauth.global.security.jwt.JwtTokenProvider
 import com.example.oauth.infrastructure.feign.client.GoogleTokenClient
 import com.example.oauth.infrastructure.feign.client.GoogleUserInfoClient
 import com.example.oauth.infrastructure.feign.dto.response.GoogleUserInfoElement
 import com.example.oauth.infrastructure.feign.dto.response.TokenResponse
 import com.example.oauth.infrastructure.feign.properties.GoogleFeignProperties
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import java.util.*
 
@@ -19,7 +21,9 @@ class GoogleOauthService(
     private val googleUserInfoClient: GoogleUserInfoClient,
     private val googleFeignProperties: GoogleFeignProperties,
     private val userRepository: UserRepository,
-    private val jwtTokenProvider: JwtTokenProvider
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val passwordEncoder: PasswordEncoder,
+    private val userFacade: UserFacade
 ) {
     companion object {
         const val GRANT_TYPE = "authorization_code"
@@ -34,13 +38,13 @@ class GoogleOauthService(
 
         val userInfo: GoogleUserInfoElement = googleUserInfoClient.getUserInfo(googleToken).response
 
-        var user: User? = userRepository.findByEmail(userInfo.email)
+        var user: User? = userFacade.getByEmail(userInfo.email)
 
         if (user == null) {
             user = User(
                 id = UUID(0, 0),
                 email = userInfo.email,
-                password = userInfo.password,
+                password = passwordEncoder.encode(userInfo.password),
                 name = userInfo.name,
                 role = USER,
                 providerType = GOOGLE,
