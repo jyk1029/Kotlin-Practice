d5c12b1be4052c1e323a2230fd8af3bf141121a3
주영 <jyk102999@dsm.hs.kr>
2023-03-02 12:27:29 +0900

    ⚡️ :: SwaggerConfig 설정

---
+++ Oauth/src/main/kotlin/com/example/oauth/infrastructure/feign/config/SwaggerConfig.kt
@@ -0,0 +1,43 @@
+package com.example.oauth.infrastructure.feign.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+
+@EnableWebMvc
+@Configuration
+class SwaggerConfig {
+    @Bean
+    fun swaggerApi(): Docket = Docket(DocumentationType.OAS_30)
+        .consumes(getConsumeContentTypes())
+        .produces(getProduceContentTypes())
+        .apiInfo(swaggerInfo())
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.colabear754.swagger_example.controllers"))
+        .paths(PathSelectors.any())
+        .build()
+        .useDefaultResponseMessages(false)
+
+    private fun swaggerInfo() = ApiInfoBuilder()
+        .title("OAuth Login Practice")
+        .description("OAuth 로그인 구현 연습")
+        .version("1.0.0")
+        .build()
+
+    private fun getConsumeContentTypes(): Set<String> {
+        val consumes = HashSet<String>()
+        consumes.add("multipart/form-data")
+        return consumes
+    }
+
+    private fun getProduceContentTypes(): Set<String> {
+        val produces = HashSet<String>()
+        produces.add("application/json;charset=UTF-8")
+        return produces
+    }
+}
