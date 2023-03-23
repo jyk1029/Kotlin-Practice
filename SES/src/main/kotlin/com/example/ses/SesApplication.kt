3debd815ba9e495c91b2c11981007094145f0b9e
Juyeong <103026521+jyk1029@users.noreply.github.com>
2023-03-23 12:18:43 +0900

    ⚙️ :: 프로젝트 설정

---
+++ SES/src/main/kotlin/com/example/ses/SesApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ses
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SesApplication
+
+fun main(args: Array<String>) {
+    runApplication<SesApplication>(*args)
+}
